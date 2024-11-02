e6141ebfa1fbf3fe3f3c484b4b8e1cb7a16f0bd7
MehmetBozkir <mehmetsalihbozkir@gmail.com>
2024-11-02 23:27:09 +0300

    Create Section1 & Added Necessary Images

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/layout/Navbar";
+import Section1 from "./components/layout/Section1";
 
 function page() {
   return (
     <div>
       <Navbar />
+      <Section1 />
     </div>
   );
 }
